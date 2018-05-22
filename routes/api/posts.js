f553f7a08d0e6329b3a39b90f653688e126895ce
Alvin Kang <oneinallin1@gmail.com>
2018-05-22 09:46:46 -0700

    Add route for creating post along with validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Validation
+const validatePostInput = require("../../validation/post");
+
+// Load Post model
+const Post = require("../../models/Post");
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
 // @access  Public
-router.get('/test', (req, res) => {
+router.get("/test", (req, res) => {
   return res.json({
     msg: "Posts Works"
   });
 });
+
+// @route   POST api/posts/
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    // Save to DB
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
