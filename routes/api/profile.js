90fa8a2867f669abd8206f21d32b22d481b9c8e0
Alvin Kang <oneinallin1@gmail.com>
2018-05-22 09:08:07 -0700

    Add route to DELETE experience, education based on exp_id, edu_id respectively

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -245,4 +245,56 @@ router.post(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Look up user profile based on user id (from header)
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // Get remove index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+
+      // Remove 1 element from removeIndex
+      profile.experience.splice(removeIndex, 1);
+
+      // Save to DB
+      profile
+        .save()
+        .then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Look up user profile based on user id (from header)
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // Get remove index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+
+      // Remove 1 element from removeIndex
+      profile.education.splice(removeIndex, 1);
+
+      // Save to DB
+      profile
+        .save()
+        .then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+  }
+);
+
 module.exports = router;
