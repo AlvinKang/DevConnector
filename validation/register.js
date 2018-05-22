d6dfe882d8e8867cfaa9a3c6d0b314667dfdbbd7
Alvin Kang <oneinallin1@gmail.com>
2018-05-22 01:16:04 -0700

    Fix validation logic order

--- validation/register.js
+++ validation/register.js
@@ -13,26 +13,31 @@ module.exports = function validateRegisterInput(data) {
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
-  } else if (Validator.isEmpty(data.name)) {
+  }
+
+  if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
+  if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
-
-  if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is required";
-  } else if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
 
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = "Confirm password field is required";
-  } else if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
   }
 
   return {
