7a5f69da33f827469464aeb43e6c58a85cf9b267
Alvin Kang <oneinallin1@gmail.com>
2018-05-21 23:26:52 -0700

    Add route /users/current which returns user info after successful validation via passport bearer token authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load User model
 const User = require("../../models/User");
@@ -69,7 +70,7 @@ router.post("/login", (req, res) => {
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
         // User matched
-        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JTW payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
 
         // Sign token
         jwt.sign(
@@ -89,5 +90,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
